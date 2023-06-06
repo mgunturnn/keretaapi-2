44dbdf5f63c6f5087b1a2141a6da87a1b1e54e37
Mohammad Guntur Nugraha <guntur.nugraha@upi.edu>
2023-06-06 21:00:49 +0700

    Update q_login.php

--- q_login.php
+++ q_login.php
@@ -11,19 +11,14 @@ $cek = mysqli_num_rows($result);
 	if($cek > 0){
 		$row = mysqli_fetch_array($result,MYSQLI_NUM);
 		$id = $row[0];
-		$level = $row[3];
 
 		
 		session_start();
 		$_SESSION['user_id'] = $id;
 		$_SESSION['email'] = $email;
 		$_SESSION['status'] = 1;
-		$_SESSION['level'] = $level;
-		if ($level === 'admin') {
-			header('Location: dashboard/index.php');
-		}else{
-			header('Location: index.php');
-		}
+		
+		header('Location: index.php');
 	}else{
 		header('Location: login.php');
 	}
